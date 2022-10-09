bc7ce4606d1fc9bae5d61bafdbcfdee04cee98e0
Djones A. Boni <djboni@gmail.com>
2022-10-09 18:12:11 -0300

    Create vscode-settings.js

---
+++ vscode-settings.js
@@ -0,0 +1,139 @@
+{
+    // Debloat
+    "telemetry.telemetryLevel": "off",
+    "extensions.ignoreRecommendations": true,
+
+    // Quality of life
+    "search.followSymlinks": false,
+    "editor.rulers": [
+        { "column": 72, "color": "#383838" },
+        { "column": 80, "color": "#606060" },
+        { "column": 88, "color": "#383838" }
+    ],
+    "editor.minimap.maxColumn": 80,
+    "files.autoSave": "onFocusChange",
+    "files.eol": "\n",
+    "files.insertFinalNewline": true,
+    "files.trimFinalNewlines": true,
+    "files.trimTrailingWhitespace": true,
+    "editor.detectIndentation": false,
+    "editor.hover.delay": 1500,
+    "editor.acceptSuggestionOnEnter": "off", // TAB accepts suggestion
+    "editor.find.addExtraSpaceOnTop": false,
+    "workbench.editor.openPositioning": "last",
+
+    // Formatter
+    "editor.formatOnSave": true,
+    "python.formatting.provider": "black",
+    "python.formatting.blackArgs": ["--line-length=80"],
+
+    // C/C++, CMake
+    "C_Cpp.autoAddFileAssociations": false,
+    "cmake.configureOnOpen": false,
+    "cmake.configureOnEdit": false,
+    "cmake.buildDirectory": "${workspaceFolder}/build_vs",
+
+    // Font https://github.com/tonsky/FiraCode
+    "editor.fontFamily": "Fira Code",
+    "editor.fontLigatures": true,
+
+    // Git, GitLens
+    "diffEditor.wordWrap": "on",
+	"diffEditor.ignoreTrimWhitespace": false,
+    "git.enableSmartCommit": false,
+    "git.confirmSync": false,
+    "gitlens.hovers.currentLine.over": "line",
+    "gitlens.currentLine.enabled": false,
+    "gitlens.codeLens.enabled": false,
+    "gitlens.statusBar.enabled": false,
+    "gitlens.defaultDateFormat": null,
+
+    // Coverage Gutters (extension)
+    "coverage-gutters.showLineCoverage": false,
+    "coverage-gutters.showGutterCoverage": true,
+
+    // Vim (extension)
+    "editor.lineNumbers": "relative",
+    "vim.easymotion": true,
+    "vim.incsearch": true,
+    "vim.hlsearch": true,
+    "vim.useCtrlKeys": true,
+    "vim.insertModeKeyBindingsNonRecursive": [
+        {
+            // Fast kj means ESC
+            // VIMRC: inoremap kj <esc>
+            "before": ["k", "j"],
+            "after": ["<esc>"]
+        },
+        {
+            // Fast KJ means ESC
+            // VIMRC: inoremap KJ <esc>
+            "before": ["K", "J"],
+            "after": ["<esc>"]
+        },
+        {
+            // Ctrl-J converts word to uppercase
+            // Type the word and Ctrl+J while in insert mode
+            // Example: i#define very_long_macro_name<Ctrl+j> 1
+            // VIMRC: inoremap <c-j> <esc>gUiwea
+            "before": ["<c-j>"],
+            "after": ["<esc>", "g", "U", "i", "w", "e", "a"]
+        },
+    ],
+    "vim.visualModeKeyBindingsNonRecursive": [
+        {
+            // Add parenthesis around selected text
+            // Example: vee(
+            // VIMRC: vnoremap ( di()<esc>P
+            "before": ["("],
+            "after": ["d", "i", "(", ")", "<esc>", "P"]
+        },
+        {
+            // Add brackets around selected text
+            // Example: vee[
+            // VIMRC: vnoremap [ di[]<esc>P
+            "before": ["["],
+            "after": ["d", "i", "[", "]", "<esc>", "P"]
+        },
+        {
+            // Add braces around selected text
+            // Example: vee{
+            // VIMRC: vnoremap { di{}<esc>P
+            "before": ["{"],
+            "after": ["d", "i", "{", "}", "<esc>", "P"]
+        },
+        {
+            // Add single quotes around selected text
+            // Example: vee"
+            // VIMRC: vnoremap ' di''<esc>P
+            "before": ["'"],
+            "after": ["d", "i", "'", "'", "<esc>", "P"]
+        },
+        {
+            // Add double quotes around selected text
+            // Example: vee"
+            // VIMRC: vnoremap " di""<esc>P
+            "before": ["\""],
+            "after": ["d", "i", "\"", "\"", "<esc>", "P"]
+        },
+        {
+            // Add back ticks around selected text
+            // Example: vee`
+            // VIMRC: vnoremap ` di``<esc>P
+            "before": ["`"],
+            "after": ["d", "i", "`", "`", "<esc>", "P"]
+        },
+    ],
+    "vim.handleKeys": {
+        "<C-f>": false, // Find
+        "<C-h>": false, // Replace
+        "<C-c>": false, // Copy
+        "<C-v>": false, // Paste
+        "<C-x>": false, // Cut
+        "<C-w>": false, // Close window
+        "<C-b>": false, // Open/close side bar
+    },
+
+    // Other
+    // ...
+}
